c66df20acf615cc7817d35c88c3133ed00928bed
Julien <juliencaron@protonmail.com>
2020-01-23 19:47:30 +0100

    Add optional query params for GET user log

--- src/controllers/exercise.ts
+++ src/controllers/exercise.ts
@@ -44,27 +44,51 @@ export function getLog(req: Request, res: Response) {
     res.status(404).json({ error: 'UserId is required' })
   }
 
-  Exercise.find({ userId: req.query.userId })
-    .then((exercises) => {
-      User.findOne({ _id: req.query.userId })
-        .then((user) => {
-          if (!user || user === null) {
-            res.status(404).json({ message: 'User non existant' })
-          }
+  interface ExerciseQuery {
+    userId: string
+    date?: {
+      $gte?: string
+      $lt?: string
+    }
+  }
 
-          const _id = user && user._id ? user._id : null
-          const username = user && user.username ? user.username : null
+  User.findOne({ _id: req.query.userId })
+    .then((user) => {
+      if (!user || user === null) {
+        res.status(404).json({ message: 'User non existant' })
+      }
 
-          if (_id && username) {
+      const _id = user && user._id ? user._id : null
+      const username = user && user.username ? user.username : null
+      const exerciseQuery: ExerciseQuery = { userId: req.query.userId }
+
+      let date = {}
+      if (req.query.from) {
+        date = { ...date, $gte: req.query.from }
+      }
+
+      if (req.query.to) {
+        date = { ...date, $lt: req.query.to }
+      }
+
+      if (date && date !== {}) {
+        exerciseQuery.date = date
+      }
+
+      if (_id && username) {
+        Exercise
+          .find(exerciseQuery)
+          .limit(parseInt(req.query.limit || 0, 10))
+          .then((exercises) => {
             res.status(200).json({
               _id,
               username,
               count: exercises.length,
               log: exercises,
             })
-          }
-        })
-        .catch((error) => res.status(500).json({ error }))
+          })
+          .catch((error) => res.status(500).json({ error }))
+      }
     })
     .catch((error) => res.status(500).json({ error }))
 }
